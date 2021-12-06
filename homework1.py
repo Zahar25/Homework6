2e9fdf68fda6233e4f4474c983f01c388abe4b8a
Zahar Pyshny <56980078+Zahar25@users.noreply.github.com>
2021-12-06 13:33:26 +0200

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -1,7 +1,9 @@
+import time
 def lights():
     auto_traffic = ['Red', 'Red', 'Red', 'Red', 'Yellow', 'Yellow', 'Green', 'Green', 'Green', 'Green', 'Yellow', 'Yellow']
     people_traffic = ['Green', 'Green', 'Green', 'Green', 'Green', 'Green', 'Red', 'Red', 'Red', 'Red', 'Red','Red']
     while True:
         for i in range(len(auto_traffic)):
             print (auto_traffic[i], people_traffic[i])
+            time.sleep(1)
 lights()
